534e2cb8649d1f1731035b3c44ed027f86da7187
freddygbaguidi08-del <freddygbaguidi08@gmail.com>
2026-06-09 13:52:07 +0200

    Delete prospecta-v2/prospecta-v3 directory

--- prospecta-v2/prospecta-v3/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { redirect } from 'next/navigation';
-import { getSession } from '@/lib/auth';
-
-export default async function RootPage() {
-  const session = await getSession();
-  if (session) redirect('/dashboard');
-  else redirect('/login');
-}
